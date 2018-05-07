8ca103c0f6eb2c97ffad53a4d2b5090775ad511d
Grant <grant@tuxinator.net>
2018-05-06 20:30:10 -0400

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     author='Grant Gangi',
     author_email='grant@tuxinator.net',
-    url='https://github.com/Grant1219/tuxinator_api/'
+    url='https://github.com/Grant1219/tuxinator_api/',
     packages=['tuxinator_api'],
     include_package_data=True,
     install_requires=[
